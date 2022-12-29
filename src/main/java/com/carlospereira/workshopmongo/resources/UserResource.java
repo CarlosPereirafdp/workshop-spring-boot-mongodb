fb71b9640a03d6ba8a78c18462177abd4aacd4b2
Carlos Pereira <carlosfcdp94@hotmail.com>
2022-12-29 18:16:07 +0000

    Create user service and repository

--- src/main/java/com/carlospereira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/carlospereira/workshopmongo/resources/UserResource.java
@@ -1,23 +1,24 @@
 package com.carlospereira.workshopmongo.resources;
 
 import com.carlospereira.workshopmongo.domain.User;
+import com.carlospereira.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User carlos = new User("1", "Carlos", "teste@gmail");
-        User joao = new User("2", "Joao", "teste@gmail");
-        return ResponseEntity.ok().body(new ArrayList<>(Arrays.asList(carlos, joao)));
+        return ResponseEntity.ok().body(userService.findAll());
     }
 }
